f6dea9552274addb978a79474cc8743062c47e24
Azusukii <dungnguyenduc2006@gmail.com>
2025-03-18 11:06:05 +0700

    Add project files.

---
+++ paddle.cpp
@@ -0,0 +1,25 @@
+#include "paddle.hpp"
+
+Paddle::Paddle(int x, int y) {
+	m_position.x = x;
+	m_position.y = y;
+	m_position.w = 50;
+	m_position.h = 100;
+}
+
+void Paddle::handle_input(SDL_Event const& event) {
+	switch (event.type) {
+	case SDL_KEYDOWN: 
+		Uint8 const* keys = SDL_GetKeyboardState(nullptr);
+
+		if(m_type == )
+	}
+}
+
+void Paddle::update(double delta_time) {
+	
+}
+
+void Paddle::draw() {
+
+}
---
+++ pong.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <SDL.h>
+#include "paddle.hpp"
+class Pong {
+public: 
+	Pong();
+	~Pong() = default;
+
+	void game_loop();
+	void update(double delta_time);
+	void draw();
+
+private:
+	SDL_Window* m_game_window;
+	SDL_Renderer* m_game_window_renderer;
+	SDL_Event m_game_window_event;
+
+	Paddle m_left_paddle;
+	Paddle m_right_paddle;
+};
